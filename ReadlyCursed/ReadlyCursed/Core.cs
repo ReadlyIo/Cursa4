b63302ee668fa9ab63e1703d3284a708a084edb8
Ваше имя <nothere@inthe.net>
2021-06-26 10:07:01 +0300

    1

---
+++ ReadlyCursed/ReadlyCursed/Core.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadlyCursed
+{
+    class Core
+    {
+        public static megorovEntities DB = new megorovEntities();
+
+    }
+}
